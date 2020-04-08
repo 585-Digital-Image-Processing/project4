cb92163b7d11a18ce2c9906d2e82a5ab3c8b7175
jiuchao <jiuchao1@gmail.com>
2020-04-08 17:38:25 -0400

    change h

--- main.m
+++ main.m
@@ -1,46 +1,66 @@
 
-%%
-F = 0.059;
-theta = 135;
-sigma = 8;
-hx_score = zeros(1,512);
-hy_score = zeros(1,512);
-for i = 1:512
-    hx_score(i) = hx(F, theta, sigma,i);
-    hy_score(i) = hy(F, theta, sigma,i);
+%% texture 1
+F = 0.042;
+theta = 0;
+sigma = 24;
+hx_score = zeros(1,4*sigma+1);
+hy_score = zeros(1,4*sigma+1);
+for i = 1:4*sigma+1
+    hx_score(i) = hx(F, theta, sigma,i-2*sigma-1);
+    hy_score(i) = hy(F, theta, sigma,i-2*sigma-1);
 end
 
 img1 = imread('texture1.gif');
 img1 = cast(img1, 'double');
-img1 = img1*255;
 imshow(img1);
 title('texture1');
 [m,n] = size(img1);
-new_img1 = img1;
+new_img1 = zeros(m,n);
 for i = 2*sigma+1:m-sigma*2
     for j = 2*sigma+1:n - sigma*2 
         original = img1(i,j);
-        img1(i,j) = hx_score(i-2*sigma:i+2*sigma)*img1(i-2*sigma:i+2*sigma,j);
-        new_img1(i,j) = img1(i, j-2*sigma:j+2*sigma)*hy_score(j-2*sigma:j+2*sigma)';
+        img1(i,j) = hx_score(1,:)*img1(i-2*sigma:i+2*sigma,j);
+        new_img1(i,j) = img1(i, j-2*sigma:j+2*sigma)*hy_score(1,:)';
         new_img1(i,j) = abs(new_img1(i,j));
         img1(i,j) = original;
     end
 end
 
-for i = 1:sigma*2-1
-    for j = 1:sigma*2-1       
-        new_img1(i,j) = 0;
-    end
-end
-
-for i = m-sigma*2+1:m
-    for j = n-sigma*2+1:n     
-        new_img1(i,j) = 0;
-    end
-end
-
-imshow(new_img1);
+%imshow(new_img1);
 surf(1:512, 1:512, new_img1);
+xlabel('x');
+ylabel('y');
+zlabel('m(x,y)');
+
+%% texture2
+F = 0.042;
+theta = 0;
+sigma = 24;
+hx_score = zeros(1,4*sigma+1);
+hy_score = zeros(1,4*sigma+1);
+for i = 1:4*sigma+1
+    hx_score(i) = hx(F, theta, sigma,i-2*sigma-1);
+    hy_score(i) = hy(F, theta, sigma,i-2*sigma-1);
+end
+
+img2 = imread('texture2.gif');
+img2 = cast(img2, 'double');
+imshow(img2);
+title('texture2');
+[m,n] = size(img2);
+new_img2 = zeros(m,n);
+for i = 2*sigma+1:m-sigma*2
+    for j = 2*sigma+1:n-sigma*2 
+        original = img2(i,j);
+        img2(i,j) = hx_score(1,:)*img2(i-2*sigma:i+2*sigma,j);
+        new_img2(i,j) = img2(i, j-2*sigma:j+2*sigma)*hy_score(1,:)';
+        new_img2(i,j) = abs(new_img2(i,j));
+        img2(i,j) = original;
+    end
+end
+
+%imshow(new_img1);
+surf(1:512, 1:512, new_img2);
 xlabel('x');
 ylabel('y');
 zlabel('m(x,y)');
